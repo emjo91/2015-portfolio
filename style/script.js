894fb39f34f5bfa25876d887c160a4a1b31017cd
EmilyJohnson <3mle33@gmail.com>
2015-04-10 09:47:58 -0500

    Got a basic layout figured out

--- style/script.js
+++ style/script.js
@@ -1,1 +1,7 @@
 // This page is hooked up correctly :) tested with an alert.
+
+window.onload function(){
+    window.addEventListener("keypress", myAlert, false);
+    
+    var myAlert = alert("YAY!");
+};
